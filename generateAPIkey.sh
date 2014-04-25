7c56a7aaffb4931699f6319cd435d32385e46ee6
Massimo Siani <massimo.siani@skysql.com>
2014-04-25 04:54:22 -0400

    Small script restyling

--- generateAPIkey.sh
+++ generateAPIkey.sh
@@ -2,7 +2,7 @@
 #
 # Part of MariaDB Manager package
 #
-# This file is distributed as part of the SkySQL MariaDB Manager package.
+# This file is distributed as part of the MariaDB Manager package.
 # It is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation, version 2.
@@ -16,10 +16,10 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-# Copyright 2013-2014 (c) SkySQL Ab
+# Copyright 2013-2014 (c) SkySQL Corporation Ab
 #
 # Author      : Massimo Siani
-# Version     : 1.0
+# Version     : 1.1
 # Date        : December 2013
 # Description    : Generates a new API ID/key pair
 #
@@ -33,16 +33,20 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
-
-# Generating API key
-newKey=$(echo $RANDOM$(date)$RANDOM | md5sum | cut -f1 -d" ")
-
 componentID=$1
 componentFile=/etc/mariadbmanager/manager.ini
+
+# Generating API key
+touch $componentFile
+grep "\[apikeys\]" ${componentFile} &>/dev/null
+if [ "$?" != "0" ] ; then
+    echo "[apikeys]" >> $componentFile
+fi
+newKey=$(echo $RANDOM$(date)$RANDOM | md5sum | cut -f1 -d" ")
 keyString="${componentID} = \"${newKey}\""
 
 # Registering key
 grep "^${componentID} = \"" ${componentFile} &>/dev/null
 if [ "$?" != "0" ] ; then
-	sed -i "/^\[apikeys\]/a $keyString" ${componentFile}
+    sed -i "/^\[apikeys\]/a $keyString" $componentFile
 fi
